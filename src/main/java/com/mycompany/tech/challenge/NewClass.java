a9170e557c8f0fd5652c23ac5fbfefcc4a442bc7
Yesith Valencia <jv@zyos.co>
2021-07-25 19:03:31 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/tech/challenge/NewClass.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tech.challenge;
+
+/**
+ *
+ * @author Equipo 30
+ */
+public class NewClass {
+    
+    private Integer version;
+    
+    public NewClass(){
+        this.version = 0;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+    
+    
+    
+}
